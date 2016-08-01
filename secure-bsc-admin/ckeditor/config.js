9382712eb71fcc50f63082757331002437bd99d7
johnvogen <john.vogen@verizon.net>
2016-08-01 10:08:06 -0400

    update

--- secure-bsc-admin/ckeditor/config.js
+++ secure-bsc-admin/ckeditor/config.js
@@ -1,47 +1,36 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function (config) {
-    // Define changes to default configuration here.
-    // For complete reference see:
-    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-    // The toolbar groups arrangement, optimized for two toolbar rows.
-    config.toolbarGroups = [
-		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
-		{ name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'links' },
 		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
-		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
 		{ name: 'styles' },
 		{ name: 'colors' },
+		{ name: 'tools' },
+		{ name: 'others' },
 		{ name: 'about' }
 	];
 
-    // Remove some buttons provided by the standard plugins, which are
-    // not needed in the Standard(s) toolbar.
-    config.removeButtons = 'Underline,Subscript,Superscript';
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
 
-    // Set the most common block elements.
-    config.format_tags = 'p;h1;h2;h3;pre';
-
-    // Simplify the dialog windows.
-    config.removeDialogTabs = 'image:advanced;link:advanced';
-
-    //config.contentsCss = 'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
-    //config.contentsCss = 'ckeditor/bootstrap.css';
-    config.contentsCss = ['/secure.bscbenefitsconnect.com/ckeditor/bootstrap.css', '/secure.bscbenefitsconnect.com/css/custom.css'];
-    //config.contentsCss = '/secure.bscbenefitsconnect.com/css/custom.css';
-    //config.stylesSet = [];
-
-
-    config.height = '600px';
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+	config.contentsCss = ['/secure-bsc-admin/ckeditor/bootstrap.css', '/secure-bsc-admin/css/custom.css'];
+    
 };
